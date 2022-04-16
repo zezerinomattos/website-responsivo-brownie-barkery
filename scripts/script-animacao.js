f68f26ce9a107e8cdf8d8ace9f9134a93aef3a0b
zezerinodev <mattoszz@hotmail.com>
2022-04-15 22:34:32 -0300

    Mudando a animação da seção de apresentação

--- scripts/script-animacao.js
+++ scripts/script-animacao.js
@@ -4,13 +4,20 @@
 
 //Deixando os elementos com opacidade 0
 $('section#apresentacao h1, section#apresentacao p, section#apresentacao hr').css('opacity', 0);
+
 $('section#apresentacao div#apresentacao-principal').css('opacity', 0);
+
+$('section#apresentacao div#apresentacao-inf-esquerda').css('opacity', 0);
+
+$('section#apresentacao div#apresentacao-inf-centro').css('opacity', 0);
+
+$('section#apresentacao div#apresentacao-inf-direita').css('opacity', 0);
 
 $('section#apresentacao h1').waypoint(function(direcao){
     if(direcao == 'down'){
         $('section#apresentacao h1, section#apresentacao p, section#apresentacao hr').addClass('animate__animated animate__fadeInUp animate__slow');
     }else{
-        $('section#apresentacao h1, p, hr').removeClass('animate__animated animate__fadeInUp animate__slow');
+        $('section#apresentacao h1, section#apresentacao p, section#apresentacao hr').removeClass('animate__animated animate__fadeInUp animate__slow');
     }
 }, {
     offset:'500px;'
@@ -26,13 +33,22 @@ $('section#apresentacao h1').waypoint(function(direcao){
     offset:'500px;'
 })
 
-$('section#apresentacao h1').waypoint(function(direcao){
+$('section#apresentacao div#apresentacao-principal').waypoint(function(direcao){
     if(direcao == 'down'){
-        $('section#apresentacao div#apresentacao-inf, section#apresentacao hr').addClass('animate__animated animate__fadeInUpBig animate__slower');
+        $('section#apresentacao div#apresentacao-inf-esquerda').addClass('animate__animated animate__fadeInLeft animate__slower');
+
+        $('section#apresentacao div#apresentacao-inf-centro').addClass('animate__animated animate__fadeInUpBig animate__slow');
+
+        $('section#apresentacao div#apresentacao-inf-direita').addClass('animate__animated animate__fadeInRight animate__slower');
+
     }else{
-        $('section#apresentacao div#apresentacao-inf, section#apresentacao hr').removeClass('animate__animated animate__fadeInUpBig animate__slower');
+        $('section#apresentacao div#apresentacao-inf-esquerda').removeClass('animate__animated animate__fadeInLeft animate__slower');
+
+        $('section#apresentacao div#apresentacao-inf-centro').removeClass('animate__animated animate__fadeInUpBig animate__slower');
+
+        $('section#apresentacao div#apresentacao-inf-direita').removeClass('animate__animated animate__fadeInRight animate__slower');
     }
 }, {
-    offset:'500px;'
+    offset:'600px;'
 })
 
