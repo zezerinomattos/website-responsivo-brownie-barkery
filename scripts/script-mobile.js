f68386aebf0903cf9eefa9624c68b7fd23f25b17
zezerinodev <mattoszz@hotmail.com>
2022-04-21 23:08:12 -0300

    Icone de X colocado no menu mobile

--- scripts/script-mobile.js
+++ scripts/script-mobile.js
@@ -1,14 +1,25 @@
 // Ativando o menu mobile
 
 function mostrarMenu(){
+
     $('nav#principal-menu ul#menu-descktop').css('display', 'flex');
+
+    $('nav#principal-menu ul#icone-menu ion-icon#menu-humburguer').css('display', 'none');
+
+    $('nav#principal-menu ul#icone-menu ion-icon#menu-X').css('display', 'flex');
 
     $('nav ul#menu-descktop').addClass('animate__animated animate__fadeInRight animate__slow');
 
 }
 
 function esconderMenu(){  
+    
     $('nav#principal-menu ul#menu-descktop').css('display', 'none'); 
+
+    $('nav#principal-menu ul#icone-menu ion-icon#menu-X').css('display', 'none');
+
+    $('nav#principal-menu ul#icone-menu ion-icon#menu-humburguer').css('display', 'flex');
+
 }
 
 var controle = true;
